9b66add4366a891a63e22f289611e7a828c2293f
BCN-Asumiko <yakoshko@gmail.com>
2024-09-04 09:55:47 +0200

    feature: create entities

---
+++ somos_sansa/src/test/java/com/somos_sansa/sansa/SansaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somos_sansa.sansa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SansaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
